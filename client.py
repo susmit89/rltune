81ccab0eebe203f37539348b9d4d1979cd77b44c
susmitj <susmit.j@partner.samsung.com>
2018-10-11 13:47:55 -0700

    created db class

--- client.py
+++ client.py
@@ -2,22 +2,33 @@
 
 import pymysql
 import os
-con = pymysql.connect(host=os.environ.get('MYSQL_HOST'),
-                      port=os.environ.get('MYSQL_PORT'),
-                      user=os.environ.get('MYSQL_USER'),
-                      password=os.environ.get('MYSQL_PASS'))
 
-cur = con.cursor()
+
+
 
 
 ### GET Parameters
-cur.execute("SHOW VARIABLES")
-rows  = cur.fetchall()
+#cur.execute("SHOW VARIABLES")
+#rows  = cur.fetchall()
 
 ### GET Version
-cur.execute("SELECT @@GLOBAL.version;")
-rows  = cur.fetchall()
+#cur.execute("SELECT @@GLOBAL.version;")
+#rows  = cur.fetchall()
 
 ### GET Metrics
-cur.execute("SHOW STATUS")
-rows  = cur.fetchall()
+#cur.execute("SHOW STATUS")
+#rows  = cur.fetchall()
+
+
+class DBENGINE():
+
+    def __init__(self):
+        con = pymysql.connect(host=os.environ.get('MYSQL_HOST'),
+                              port=int(os.environ.get('MYSQL_PORT')),
+                              user=os.environ.get('MYSQL_USER'),
+                              password=os.environ.get('MYSQL_PASS'))
+        self.cur = con.cursor()
+
+    def state(self, query):
+        self.cur.execute(query)
+        return self.cur.fetchall()
