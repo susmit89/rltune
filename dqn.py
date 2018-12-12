b5620d0fae4046517680e488b644dda0136c2973
Susmit Japhalekar <susmit.j@partner.samsung.com>
2018-12-11 22:06:26 -0800

    Update dqn.py

--- dqn.py
+++ dqn.py
@@ -32,19 +32,6 @@ class DQNAgent:
         self.target_model = self._build_model()
         self.update_target_model()
 
-    """Huber loss for Q Learning
-    References: https://en.wikipedia.org/wiki/Huber_loss
-                https://www.tensorflow.org/api_docs/python/tf/losses/huber_loss
-    """
-
-    def _huber_loss(self, y_true, y_pred, clip_delta=1.0):
-        error = y_true - y_pred
-        cond  = K.abs(error) <= clip_delta
-
-        squared_loss = 0.5 * K.square(error)
-        quadratic_loss = 0.5 * K.square(clip_delta) + clip_delta * (K.abs(error) - clip_delta)
-
-        return K.mean(tf.where(cond, squared_loss, quadratic_loss))
 
     def _build_model(self):
         #randome_uniform = initializers.RandomUniform(minval=-0.05, maxval=0.05, seed=None)
